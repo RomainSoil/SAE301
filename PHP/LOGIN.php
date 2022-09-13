1980f199fd8862655a00b9dc4b4d5f30035fb89b
Romain Soil <romain.soil@uphf.fr>
2022-09-13 10:20:59 +0200

    Premier dossier php "hello word"

---
+++ PHP/LOGIN.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title> hello Word </title>
+</head>
+
+<body>
+
+<?php
+echo "Hello word ";
+echo "baptiste c'est un bg";
+?>
+
+</body>
+</html>
