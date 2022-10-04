f799618f0be83dfe9a3e6c819289b2936c8a31b0
Romain Soil <romain.soil@uphf.fr>
2022-10-04 17:53:34 +0200

    Finish 04/10

--- PHP/Accueil.php
+++ PHP/Accueil.php
@@ -8,13 +8,16 @@
 <body>
 <br><br>
 <header>
-    <img src="logoIFSI.png" alt="logo" width=150 height=150>
+    <a href="Accueil.php">
+        <img src="logoIFSI.png" width=150 height=150 alt="" >
+    </a>
     <h1> Institut de Formation aux Soins Infirmiers (IFSI)</h1>
     <br><br>
 </header>
+
 <div class="f">
     <h3>Entrez votre identifiant et votre mot de passe</h3> <br>
-<form action="Login%20Maneo.php" method="post">
+<form method="post">
      <input type="text" name="id" id="id" placeholder="Identifiant"><br><br>
     <input type="text" name="mdp" placeholder="Mot de passe"><br><br>
     <input type="submit" value="Confirmer">
@@ -41,11 +44,22 @@
 require('email.php');
 require ('MotDePasse.php');
 require('ConnectionBDD.php');
+require('Connexion.php');
 
 $conn = new ConnectionBDD();
 $pdo = $conn->connexion();
 $ClassMail = new email();
 @$ClassMail->email($_POST['id'], $_POST['mdp']);
+$ClassConn= new Connexion();
+if (@$ClassMail->email($_POST['id'])){
+    if(@$ClassConn->connexionEtu($pdo,$_POST['id'],$_POST['mdp'])) {
+        echo "c bon";
+        header('Location:PageEtu.php');
+        exit;
 
+    }
+    else
+        echo "c pas bon";
+}
 
 ?>
