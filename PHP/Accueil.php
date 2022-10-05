39d1045d31039d7da866e7c0584b9654f5aa5798
Baptiste <baptiste.tellier@uphf.fr>
2022-10-05 10:45:34 +0200

    Accueil commentaire

--- PHP/Accueil.php
+++ PHP/Accueil.php
@@ -5,6 +5,7 @@
 </head>
 <body>
 <br><br>
+<! Le haut de la page avec l'image et le titre>
 <header>
     <a href="Accueil.php">
         <img src="logoIFSI.png" width=150 height=150 alt="" >
@@ -12,7 +13,7 @@
     <h1> Institut de Formation aux Soins Infirmiers (IFSI)</h1>
     <br><br>
 </header>
-
+<! Cette classe est le formulaire de connexion au centre de la page>
 <div class="f">
     <h3>Entrez votre identifiant et votre mot de passe</h3> <br>
     <form method="post">
@@ -21,13 +22,15 @@
         <input type="submit" value="Confirmer">
     </form>
 </div>
+<! Lien en dessous de la box connexion>
+
 <div class="compte">
     <br><br><br>
     <a href="Login%20Maneo.php">créer un compte</a><br><br>
     <a href="MotDePasseOublie.php">Mot de passe oublié</a><br><br>
     <a href="https://cas.uphf.fr/login-help/">Besoin d'aide</a><br><br>
 </div>
-
+<! Le bas de l'image avec le carré rouge>
 <footer>
     <div class="sec">
     Pour des raisons de sécurité, veuillez vous déconnecter et fermer votre navigateur lorsque vous avez fini d'accéder aux services authentifiés.
@@ -44,10 +47,11 @@ require ('MotDePasse.php');
 require('ConnectionBDD.php');
 require('Connexion.php');
 
+/* La partie de la validation de connexion qui renvoie la page correspondante*/
 $conn = new ConnectionBDD();
 $pdo = $conn->connexion();
 $ClassMail = new email();
-@$ClassMail->email($_POST['id'], $_POST['mdp']);
+@$ClassMail->email($_POST['id']);
 $ClassConn= new Connexion();
 if (@$ClassMail->email($_POST['id'])){
     if(@$ClassConn->connexionEtu($pdo,$_POST['id'],$_POST['mdp'])) {
@@ -63,8 +67,8 @@ if (@$ClassMail->email($_POST['id'])){
 }
 
 ?>
-
 <script>
+    ///Boutton affiche le MDP///
     e=true;
     function changer3(){
         if(e){
