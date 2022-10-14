dd6129da260d42a19e8bf0d08b9ec4d7a8b2b206
etu <parsycharlestom@gmail.com>
2022-10-14 16:16:35 +0200

    modif barre tache scenario

---
+++ PHP/Radio.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ConnexionProfesseur</title>
+    <link rel="stylesheet" href="Scenario.css" >
+    <script src="LesFonctionsJS.js"></script>
+
+</head>
+
+<div class="fontHead">
+    <header>
+        <a href="Accueil.php">
+            <img src="logoIFSI.png" width=150 height=150 alt="" >
+        </a>
+        <h1> Institut de Formation aux Soins Infirmiers (IFSI)</h1>
+        <br>
+    </header>
+</div>
+<div class="font">
+    <div class="deco">
+        <a href="Accueil.php"> <img src="retour.png"  class="icone" width=50 height=50 alt="" > </a>
+    </div>
+</div>
+
+<div class="font">
+    <div class="btn-group">
+        <button class="button" onclick="document.location='PageProf.php'">Accueil</button>
+        <button class="button" onclick="document.location='CreateScenario.php'">Scénario</button>
+        <button class="button" onclick="document.location='Correction.php'">Correction</button>
+        <button class="button" onclick="document.location='Note.php'">Note</button>
+        <button class="button" onclick="document.location=">Message </button>
+
+    </div>
+    <br>
+</div>
+
+
+<div class="fontHead">
+    <br>
+    <div class="btn-scenario">
+        <button class="button" onclick="document.location='Patient.php'">Patient</button>
+        <button class="button" onclick="document.location='Securite.php'">Sécurité</button>
+        <button class="button" onclick="document.location='Diagnostic.php'">Diagnostic</button>
+        <button class="button" onclick="document.location='Soins.php'">Soins Relationnel</button>
+        <button class="button" onclick="document.location='Elimination.php'">Elimination </button>
+        <button class="button" onclick="document.location='Cardio.php'">Cardio </button>
+        <button class="button_select" onclick="document.location='Radio.php'">Radio </button>
+        <button class="button" onclick="document.location='Mobilite.php'">Mobilité </button>
+        <button class="button" onclick="document.location='Hygiene.php'">Hygiène </button>
+        <button class="button" onclick="document.location='Alimentation.php'">Alimentation </button>
+        <button class="button" onclick="document.location='Neuro.php'">Neuro </button>
+    </div>
+</div>
+
+
+</div>
+</body>
+</html>
+
+<?php
+?>
