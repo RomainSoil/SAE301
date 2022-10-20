aec250227b073e3787314642b94929f6eefe810d
Manéo <maneo.dangreaux@uphf.fr>
2022-10-20 10:26:19 +0200

    Choisir un patient pour ajouter une contrainte

--- PHP/ScenarioProf/CreateScenario.php
+++ PHP/ScenarioProf/CreateScenario.php
@@ -13,8 +13,6 @@ session_start()
 
 </head>
 
-
-
 <body>
 <?php
 include("BarreScenario.html");
@@ -23,11 +21,9 @@ include("BarreScenario.html");
     <h1>Création de Scénario</h1>
 </div>
 <div class="contrainte">
-    <form action="Radio.php" method="post">
-        <input type="submit" value="Ajouter une contrainte" name="Contrainte">
-    </form>
 <form action="Patient.php" method="post">
     <input type="submit" value="Créer un patient" name="Créer">
+
 </form>
 </div>
     <!--Le bas de page avec le boutton si on a besoin d'aide-->
@@ -40,7 +36,45 @@ include("BarreScenario.html");
 </body>
 </html>
 
-    <?php ?>
+
+
+    <?php
+    include ('../ConnectionBDD.php');
+    $pdo = ConnectionBDD::getInstance();
+    $bdd = $pdo->connexion();
+        $patients = $bdd->query("SELECT * FROM patient");
+
+        ?>
+    <form method="post">
+    <select name="patient">
+        <option value="2">Selectionnez un patient</option>
+            <?php
+            while ($patient = $patients->fetch()){
+                $pat = $patient[1];
+                $pat.=" ";
+                $pat.=$patient[2];
+                $pat.=" ";
+                $pat.=$patient[4];
+                ?>
+            <option value="1"><?php echo $pat?></option>
+    <?php
+            }
+            ?>
+                </select>
+        <input type="submit" value="Ajouter une contrainte" name="Contrainte">
+    </form>
+<?php
+function contrainte(){
+    if (isset($_POST['patient']) && $_POST['patient']==1){
+        if (isset($_POST['Contrainte'])) {
+            header('Location: Radio.php');
+            }
+        }
+}
+
+contrainte();
+
+    ?>
 
 
 
