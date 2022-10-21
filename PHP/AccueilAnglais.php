5d10c008d939bdf98302ffbb0364b2ac2990a8f5
Baptiste <baptiste.tellier@uphf.fr>
2022-10-21 16:45:27 +0200

    accueil en anglais

--- PHP/AccueilAnglais.php
+++ PHP/AccueilAnglais.php
@@ -1,3 +1,4 @@
+
 <?php
 session_start();
 $_SESSION['IdChat']=1;
@@ -17,39 +18,39 @@ $_SESSION['IdChat']=1;
 <!--Le haut de la page avec l'image et le titre-->
 <header>
     <div class="trad">
-        <form action="AccueilAnglais.php" method="post">
-            <input type="submit" value="Anglais">
+        <form action="Accueil.php" method="post">
+            <input type="submit" value="Francais">
         </form>
     </div>
     <a href="Accueil.php">
         <img src="image/logoIFSI.png" width=150 height=150 alt="" >
     </a>
-    <h1> Institut de Formation aux Soins Infirmiers (IFSI)</h1>
+    <h1> Institute of Nursing Training </h1>
     <br><br>
 </header>
 <!--Cette classe est le formulaire de connexion au centre de la page-->
 <div class="f">
-    <h3>Entrez votre identifiant et votre mot de passe</h3> <br>
+    <h3>Enter your login and password</h3> <br>
     <form method="post">
-        <input type="email" name="id" id="id" placeholder="Identifiant" required><br><br>
-        <input type="password" name="mdp" id="mdp" placeholder="Mot de passe"><button type="button" onclick="changer('mdp')">O</button><br><br>
-        <input type="submit" value="Confirmer">
+        <input type="email" name="id" id="id" placeholder="login" required><br><br>
+        <input type="password" name="mdp" id="mdp" placeholder="password"><button type="button" onclick="changer('mdp')">O</button><br><br>
+        <input type="submit" value="submit">
     </form>
 </div>
 <!--Lien en dessous de la box connexion-->
 
 <div class="compte">
     <br><br><br>
-    <a href="Login.php">créer un compte</a><br><br>
-    <a href="MotDePasseOublie.php">Mot de passe oublié</a><br><br>
-    <a href="https://cas.uphf.fr/login-help/">Besoin d'aide</a><br><br>
+    <a href="Login.php">create an account</a><br><br>
+    <a href="MotDePasseOublie.php">forgot password</a><br><br>
+    <a href="https://cas.uphf.fr/login-help/">need help</a><br><br>
 </div>
 <!--Le bas de l'image avec le carré rouge-->
 <footer>
     <div class="sec">
-    Pour des raisons de sécurité, veuillez vous déconnecter et fermer votre navigateur lorsque vous avez fini d'accéder aux services authentifiés.
+        For security reasons, please log out and close your browser when you are finished accessing authenticated services.
     <br>
-    Vos identifiants sont strictement confidentiels et ne doivent en aucun cas être transmis à une tierce personne.
+        Your identifiers are strictly confidential and must under no circumstances be transmitted to a third party.
 </div>
 </footer>
 </body>
